6012372e84dbecc319a7c46106e4e665a91901bc
Itzel97 <53825507+Itzel97@users.noreply.github.com>
2019-12-01 22:36:24 -0600

    java

---
+++ JavaBasic/Excersices/NumeroPar.java
@@ -0,0 +1,13 @@
+public class NumeroPar {
+	public static void main(String [] args) {
+		int valorEntero = Integer.valueOf(args[0]);
+		boolean esPar = valorEntero%2 == 0;
+		
+		if(esPar){
+			System.out.println("El numero " +  valorEntero + " es par.");
+		}
+		else{
+			System.out.println("El numero " +  valorEntero + " no es par.");
+		}
+	}
+}
--- Saludo.java
+++
@@ -1,7 +0,0 @@
-public class Saludo {
- public static void main( String [] name ){
-	System.out.print("Hola ");
-	System.out.print(name[0]);
-	System.out.print(" Como estas? ");
-    }
-}
